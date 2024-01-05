a337170ee080dc4295df3b27076d194067818e7a
ziasquinn <56662076+ziasquinn@users.noreply.github.com>
2024-01-05 12:42:12 -0500

    Add files via upload

---
+++ 5x6/keymaps/ziasquinn/keymap.c
@@ -0,0 +1,37 @@
+#include QMK_KEYBOARD_H
+#define BONGO_ENABLE
+#include "bongo.h"
+#define _QWERTY 0
+#define _LOWER 1
+
+#define LOWER MO(_LOWER)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT_5x6(
+        QK_GESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
+        KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL,
+        KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
+        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLS,
+                         KC_LEFT,KC_RGHT,                                                       KC_UP, KC_DOWN,
+                                         KC_BSPC,KC_DEL,                        KC_ENT, KC_SPC,
+                                         KC_LALT,KC_LGUI,                         LOWER,  KC_HOME,
+                                         KC_LCTL, KC_PSCR,                        LOWER, KC_END
+    ),
+
+    [_LOWER] = LAYOUT_5x6(
+        KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LBRC,KC_RBRC,KC_DEL,
+        _______,_______,_______,_______,_______,KC_LBRC,                        KC_RBRC, KC_P7 , KC_P8 , KC_P9 ,_______,KC_PLUS,
+        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_RPRN, KC_P4 , KC_P5 , KC_P6 ,KC_MINS,KC_PIPE,
+        _______,_______,_______,_______,_______,_______,                        _______, KC_P1 , KC_P2 , KC_P3 ,KC_EQL ,KC_UNDS,
+                                                _______,KC_PSCR,            _______, KC_P0,
+                                                _______,_______,            _______,_______,
+                                                _______,_______,            _______,_______,
+                                                _______,_______,            _______,_______
+
+    ),
+};
+bool oled_task_kb(void) {
+    draw_bongo();
+    return false;
+}
+
---
+++ 5x6/rules.mk
@@ -0,0 +1,20 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTLOADER = qmk-dfu
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+SPLIT_KEYBOARD = yes
+OLED_ENABLE = yes
+OLED_DRIVER_ENABLE = yes
+OLED_DRIVER = ssd1306
+VIA_ENABLE = yes
+WPM_ENABLE = yes
+LTO_ENABLE = yes
